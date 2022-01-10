77baa085fa8bae15b7fbe4be6cec540815a948d6
NickyMB <mikolajbacior@gmail.com>
2022-01-10 20:42:40 +0100

    poprawki

--- EE.09/EE.09-03-20.06-SG/kalendarz.php
+++ EE.09/EE.09-03-20.06-SG/kalendarz.php
@@ -1,73 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mój kalendarz</title>
     <link rel="stylesheet" href="styl5.css">
 </head>
-
 <body>
-
     <div class="gorny-lewy">
         <img src="logo1.png" alt="Mój kalendarz">
     </div>
-
     <div class="gorny-prawy">
         <h1>KALENDARZ</h1>
         <p>miesiąc: lipiec, rok:2020</p>
     </div>
-
-
-    <main>
+    <div class="glowny">
         <?php
-        require("connect.php");
-
-        $qr = "SELECT dataZadania, miesiac, wpis FROM `zadania` WHERE miesiac = 'lipiec'";
-        $result = $conn->query($qr);
-        while ($row = $result->fetch_assoc()) {
-            $data = $row['dataZadania'];
-            $miesiac = $row['miesiac'];
-            $wpis = $row['wpis'];
-
-            echo '<div>';
-            echo "<h6>$data, $miesiac</h6>";
-            echo "<p>$wpis</p>";
-            echo '</div>';
-        }
-
-        mysqli_close($conn)
+            $polonczenie = mysqli_connect("localhost","root","","egzamin5");
+            $zapytanie = mysqli_query($polonczenie,"SELECT dataZadania, miesiac, wpis FROM `zadania` WHERE miesiac = 'lipiec'");
+            while ($r = mysqli_fetch_array($zapytanie)) 
+            {
+                echo '<div class="gen">';
+                echo "<h6>$r[0], $r[1]</h6>";
+                echo "<p>$r[2]</p>";
+                echo '</div>';
+            }
         ?>
-    </main>
-
-
-
-
+    </div>
     <?php
-require("connect.php");
-
-    if (isset($_POST['wydarzenie'])) {
-        
+    if (isset($_POST['wydarzenie'])) 
+    {
         $wydarzenie = $_POST['wydarzenie'];
-        $qr = "UPDATE zadania SET wpis = '$wydarzenie' WHERE dataZadania = '2020-07-13'";
-        $conn->query($qr);
-        $conn->close();
+        mysqli_query($polonczenie,"UPDATE zadania SET wpis = '$wydarzenie' WHERE dataZadania = '2020-07-13'");
+        mysqli_close($polonczenie);
     }
     ?>
-
-    <footer class="stopka">
+    <div class="stopka">
         <form action="kalendarz.php" method="POST">
-            <label for="wpis">dodaj wpis:<input type="text" name="wydarzenie"> <button type="submit">DODAJ</button></label>
-            <br>
-            <br>
-            <p>Stronę wykonał: Jan Kupczyk 4TID</p>
+            <p for="wpis">dodaj wpis:<input type="text" name="wydarzenie"> <button type="submit">DODAJ</button></p>
+            <p>Stronę wykonał: 0132456789</p>
         </form>
-    </footer>
-
+    </div>
 </body>
-
 </html>
 
 
